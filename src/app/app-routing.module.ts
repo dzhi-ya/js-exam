4f7f5361f0ea3319b07b4d740913d3c8c44ba9d8
Daria <daria_zhitnaya@inbox.ru>
2021-01-25 19:14:15 +0300

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InfoComponent } from './info/info.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: InfoComponent,
+  },
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('./products/products.module').then((m) => m.ProductsModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
